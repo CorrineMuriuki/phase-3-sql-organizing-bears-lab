bb1627921fcb1923c81f576f45d2f101bbfb3886
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-11-11 23:29:54 +0300

    Completed

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears(
+    //columns here
+  id INTEGER NOT NULL PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 15, "M", "Brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 21, "M", "Dark Brown", "Hyper", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 26, "F", "Brown", "Calm", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 52, "M", "Grey", "Grumpy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 10, "F", "Dark Brown", "Hyper", 1);
+INSERT INTO bears VALUES (6,"Grinch", 34, "M", "Black", "Grumpy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 15, "F", "Brown", "Chill", 1);
+INSERT INTO bears VALUES (8,null, 30, "M", "Black", "Angry", 1);
